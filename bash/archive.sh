27591c060f35ee58d444f0bc5fa54306dff819b2
Feng Yan <alexyfyf@163.com>
2024-05-09 11:52:23 +1000

    update archive script

--- bash/archive.sh
+++ bash/archive.sh
@@ -12,36 +12,36 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-DIRECTORY=$(echo $1 | sed 's/\/$//')
+RAWDIRECTORY=$(echo $1 | sed 's/\/$//')
 
-# Step into the directory
-cd "$DIRECTORY" || exit
+# cp directory to current location in case no write permission
+DIRECTORY=$(basename $RAWDIRECTORY)
+cp -r $RAWDIRECTORY .
 
 # Create an md5 checksum file for all files in the directory
-find . -type f -exec md5sum {} + > ../"${DIRECTORY}.manifest"
+find "$DIRECTORY" -type f -exec md5sum {} + > "${DIRECTORY}.manifest"
 
-# Step back out
-cd ..
-
-# Create a tarball of the directory, including the md5 checksum file
-tar -czf "${DIRECTORY}.tar.gz" "${DIRECTORY}" "${DIRECTORY}.manifest"
+# Create a tarball of the directory
+tar -czf "${DIRECTORY}.tar.gz" "${DIRECTORY}" 
 
 # Extract the tarball to a temporary directory for verification
 mkdir -p temp_verification_dir
 tar -xzf "${DIRECTORY}.tar.gz" -C temp_verification_dir
 
 # Verify the integrity using the md5 checksum file
-cd temp_verification_dir/"$DIRECTORY" || exit
-md5sum -c "../${DIRECTORY}.manifest" > ../../${DIRECTORY}_checkresult
+cd temp_verification_dir || exit
+md5sum -c "../${DIRECTORY}.manifest" > ../${DIRECTORY}.checkresult
 
 # Cleanup: remove the temporary directory and checksum file
-cd ../..
-rm -r temp_verification_dir
+cd ..
+rm -r temp_verification_dir $DIRECTORY
 
 echo "The script has completed. Please check the output above for any discrepancies in the md5 checksum verification."
 
 echo Total file number
-cat ${DIRECTORY}_checkresult | wc -l
+cat ${DIRECTORY}.checkresult | wc -l
 
 echo Passed file number
-grep ': OK' ${DIRECTORY}_checkresult | wc -l 
+grep ': OK' ${DIRECTORY}.checkresult | wc -l 
+
+## after generate tar.gz, manifest and checkresults, generate a md5sum for the 3 files for later transfer
